a97782cff30496319ccff233f8fd05d77cdbb7f7
Vadim <vblinov2009.rus@yandex.ru>
2025-06-11 13:25:12 +0300

    swim и bike задния

--- Flask/pages/main_pages.py
+++ Flask/pages/main_pages.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_login import current_user, AnonymousUserMixin
 from Flask.database.database import User, Premium
 
@@ -14,7 +14,6 @@ def main_pages(app, session):
             fullname = name if len(name) < 30 else name[:28] + '...'
 
             meals = session.query(Premium).filter(Premium.user_id == user.id).all()
-            print(meals)
 
             data = {
                 'name': name,
@@ -23,8 +22,11 @@ def main_pages(app, session):
                 'date_registr': user.created_date.strftime('%d.%m.%Y'),
                 'dietolog_count': len(meals),
                 'percent': '0%',
-                'percent_today': '0%'
+                'percent_today': '0%',
+                'include_bicycle': user.bike,
+                'include_swim': user.swimming
             }
+            print(user.bike, user.swimming)
             if meals:
                 data['meals'] = meals[-1].text
                 data['date'] = meals[-1].date.strftime('%d.%m.%Y')
@@ -35,3 +37,18 @@ def main_pages(app, session):
             return render_template('profile.html', **data,)
         else:
             return redirect('/login')
+
+    @app.route('/update_setting', methods=['POST'])
+    def update_setting():
+        data = request.get_json()
+        setting_name = data.get('setting')
+        user = current_user
+
+        if data['setting'] == 'include_bicycle':
+            user.bike = data['value']
+        elif data['setting'] == 'include_swim':
+            user.swimming = data['value']
+
+        print(f"Получено: {setting_name} = {data['value']}")
+
+        return jsonify({"status": "success", "received": data})
