ff45cf7e8f3a7343c56070ef25e2f13d3aff773e
Vadim <vblinov2009.rus@yandex.ru>
2025-06-11 21:24:03 +0300

    кнопка выйти

--- Flask/database/constants.py
+++ Flask/database/constants.py
@@ -10,3 +10,11 @@ PASSWORD = 'E7X2A9M*P5L3E0!'
 
 load_dotenv('Flask/data/.env')
 API_KEY = os.getenv('API_KEY')
+
+ICONS = {
+    'bicycle': '<i class="fa-solid fa-bicycle"></i>',
+    'swimming': '<i class="fa-solid fa-water"></i>',
+    'running': '<i class="fa-solid fa-person-running"></i>',
+    'walking': '<i class="fa-solid fa-person-walking"></i>',
+    'other': '<i class="fa-solid fa-fire-flame-curved"></i>'
+}
